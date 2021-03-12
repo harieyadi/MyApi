2f59389f75d5cd470ce321d77d9d0c2a8816d758
harieyadi <73776729+harieyadi@users.noreply.github.com>
2021-03-12 16:25:01 +0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -360,6 +360,30 @@ router.get('/igdown', async (req, res, next) => {
 })
 })
 
+router.get('/yta', async (req, res, next) => {
+        var apikeyInput = req.query.apikey,
+            url = req.query.url
+            
+	if(!apikeyInput) return res.json(loghandler.notparam)
+	if(apikeyInput != 'SrtBot') return res.json(loghandler.invalidKey)
+    if (!url) return res.json({ status : false, creator : `${creator}`, message : "masukan parameter url"})
+
+       fetch(encodeURI(`https://mhankbarbar.herokuapp.com/api/yta?url=${url}`))
+        .then(response => response.json())
+        .then(data => {
+        var result = data;
+             res.json({
+                 result
+             })
+         })
+         .catch(e => {
+         	res.json(loghandler.error)
+})
+})
+
+
+
+
 router.get('/randomquote', async (req, res, next) => {
         var apikeyInput = req.query.apikey
             
